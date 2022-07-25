9dd76f9ee88e96a323acee50689b61fee979da54
Chris Pryer <cnpryer@gmail.com>
2022-07-24 21:32:19 -0400

    Add Config & Solver to ga

--- src/ga/mod.rs
+++ src/ga/mod.rs
@@ -1,6 +1,17 @@
+use std::error::Error;
+
 /// These ga solvers are designed to solve both constrained and unconstrained problems by encoding solution traits as
 /// *genes*, solution states as *individuals*, and solution groups as *populations*. Genetic algorithms improve on populations
 /// iteratively (referred to as *generations*) via reproduction and scoring an individual's *fitness*.
+
+const SOLVER_NAME: &str = "GeneticAlgorithm";
+
+// Default data for genetic algorithm configuration
+const DEFAULT_MINIMUM_GENERATIONS: u32 = 1000;
+const DEFAULT_SATISFACTORY_FITNESS: u32 = 0;
+const DEFAULT_CROSSOVER_RATE: f32 = 0.5;
+const DEFAULT_MUTATION_RATE: f32 = 0.05;
+const DEFAULT_SELECTION_RATE: f32 = 0.5;
 
 /// A `Gene` is the atomic structure of the evolutionary solver used to represent solution traits.
 ///
@@ -66,12 +77,81 @@ impl Model<'_> {
     }
 }
 
+// A `Config` used to configure the solve.
+struct Config {
+    // Minumum number of generations to produce
+    generations: u32,
+    // Minimum threshold for solve defined as fitness score
+    fitness_threshold: Option<u32>,
+    // Percent of parent A to crossover with parent B
+    crossover_rate: f32,
+    // Rate at which genes are psuedo-logically mutated
+    mutation_rate: f32,
+    // Rate at which indivudals are selected for reproduction
+    selection_rate: f32,
+}
+
+impl Config {
+    fn new(
+        generations: u32,
+        fitness_threshold: Option<u32>,
+        crossover_rate: f32,
+        mutation_rate: f32,
+        selection_rate: f32,
+    ) -> Self {
+        Config {
+            generations,
+            fitness_threshold,
+            crossover_rate,
+            mutation_rate,
+            selection_rate,
+        }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            generations: DEFAULT_MINIMUM_GENERATIONS,
+            fitness_threshold: Some(DEFAULT_SATISFACTORY_FITNESS),
+            crossover_rate: DEFAULT_CROSSOVER_RATE,
+            mutation_rate: DEFAULT_MUTATION_RATE,
+            selection_rate: DEFAULT_SELECTION_RATE,
+        }
+    }
+}
+
+pub struct Solver<'a> {
+    name: &'a str,
+    model: Model<'a>,
+    config: Option<Config>,
+}
+
+impl Solver<'_> {
+    fn new(model: Model<'_>, config: Option<Config>) -> Solver {
+        Solver {
+            name: SOLVER_NAME,
+            model,
+            config,
+        }
+    }
+}
+
+// TODO
+fn run(solver: &Solver) -> () {
+    ()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn test_fitness_fn(individual: Individual) -> i32 {
+        0 // TODO
+    }
+
     #[test]
-    fn gene() {
+    fn test_gene() {
         let gene = Gene::new("Test", vec![1, 2, 3]);
         let expected = vec![1, 2, 3];
 
@@ -80,7 +160,7 @@ mod tests {
     }
 
     #[test]
-    fn individual() {
+    fn test_individual() {
         let individual = Individual::new(vec![
             Gene::new("Gene0", vec![1, 2, 3]),
             Gene::new("Gene1", vec![1, 2, 3]),
@@ -99,7 +179,7 @@ mod tests {
     }
 
     #[test]
-    fn population() {
+    fn test_population() {
         let population = Population::new(vec![
             Individual::new(vec![
                 Gene::new("Gene0", vec![1, 2, 3]),
@@ -124,11 +204,7 @@ mod tests {
     }
 
     #[test]
-    fn model() {
-        fn fitness(individual: Individual) -> i32 {
-            0 // TODO
-        }
-
+    fn test_model() {
         let model = Model::new(
             Population::new(vec![
                 Individual::new(vec![
@@ -140,7 +216,7 @@ mod tests {
                     Gene::new("Gene1", vec![1, 2, 3]),
                 ]),
             ]),
-            &fitness,
+            &test_fitness_fn,
         );
 
         // TOOD: model validity
@@ -158,5 +234,39 @@ mod tests {
             (model.fitness)(Individual::new(vec![Gene::new("Gene0", vec![1, 2, 3])])),
             0
         );
+    }
+
+    #[test]
+    fn test_config() {
+        let cfg = Config::default();
+
+        assert_eq!(cfg.crossover_rate, DEFAULT_CROSSOVER_RATE);
+        assert_eq!(cfg.fitness_threshold, Some(DEFAULT_SATISFACTORY_FITNESS));
+        assert_eq!(cfg.generations, DEFAULT_MINIMUM_GENERATIONS);
+        assert_eq!(cfg.mutation_rate, DEFAULT_MUTATION_RATE);
+        assert_eq!(cfg.selection_rate, DEFAULT_SELECTION_RATE);
+    }
+
+    #[test]
+    fn test_solver() {
+        let model = Model::new(
+            Population::new(vec![
+                Individual::new(vec![
+                    Gene::new("Gene0", vec![1, 2, 3]),
+                    Gene::new("Gene1", vec![1, 2, 3]),
+                ]),
+                Individual::new(vec![
+                    Gene::new("Gene0", vec![1, 2, 3]),
+                    Gene::new("Gene1", vec![1, 2, 3]),
+                ]),
+            ]),
+            &test_fitness_fn,
+        );
+        let test_solver = Solver::new(model, None);
+
+        // TODO
+        run(&test_solver);
+
+        assert_eq!(test_solver.name, SOLVER_NAME);
     }
 }
