7550226bbe26b00a00deeabb3a8e30fda4a34a23
Chris Pryer <cnpryer@gmail.com>
2022-07-24 20:41:47 -0400

    Add more attributes to ga structs
    gene names, individual fitness scores, and population generation number

--- src/ga/mod.rs
+++ src/ga/mod.rs
@@ -1,61 +1,64 @@
 /// These ga solvers are designed to solve both constrained and unconstrained problems by encoding solution traits as
 /// *genes*, solution states as *individuals*, and solution groups as *populations*. Genetic algorithms improve on populations
 /// iteratively (referred to as *generations*) via reproduction and scoring an individual's *fitness*.
-///
-/// `Gene`
+
 /// A `Gene` is the atomic structure of the evolutionary solver used to represent solution traits.
 ///
 /// For example, in scheduling problems a gene could represent time, duration, etc..
-///
-/// `Individual`
-/// An `Individual` is a structure that represents a solution's composition. `Individual`s are composed of `Gene`s.
-///
-/// For example, in scheduling problems an individual could represent a schedule.
-///
-/// `Population`
-/// A `Population` is a group of `Individual`s.
-///
-/// `Model`
-/// A `Model` is a structure that defines the problem to be solved.
-pub struct Gene {
+pub struct Gene<'a> {
+    name: &'a str,
     data: Vec<u16>,
 }
 
-impl Gene {
-    fn new(data: Vec<u16>) -> Gene {
-        Gene { data }
+impl Gene<'_> {
+    fn new(name: &str, data: Vec<u16>) -> Gene {
+        Gene { name, data }
     }
 }
 
-pub struct Individual {
-    genes: Vec<Gene>,
+/// An `Individual` is a structure that represents a solution's composition. `Individual`s are composed of `Gene`s.
+///
+/// For example, in scheduling problems an individual could represent a schedule.
+pub struct Individual<'a> {
+    // Ordered (TODO) vector of genes. Each individual in a population must have the same types of genes
+    genes: Vec<Gene<'a>>,
+    fitness: Option<i32>,
 }
 
-impl Individual {
+impl Individual<'_> {
     fn new(genes: Vec<Gene>) -> Individual {
-        Individual { genes }
+        Individual {
+            genes,
+            fitness: None,
+        }
     }
 }
 
-pub struct Population {
-    individuals: Vec<Individual>,
+/// A `Population` is a group of `Individual`s.
+pub struct Population<'a> {
+    generation: u32,
+    individuals: Vec<Individual<'a>>,
 }
 
-impl Population {
+impl Population<'_> {
     fn new(individuals: Vec<Individual>) -> Population {
-        Population { individuals }
+        Population {
+            generation: 0,
+            individuals,
+        }
     }
 }
 
+/// A `Model` is a structure that defines the problem to be solved.
 pub struct Model<'a> {
     // first generation of individuals
-    population: Population,
+    population: Population<'a>,
     // fitness evaluation function that evaluates an Individual
     fitness: &'a dyn Fn(Individual) -> i32,
 }
 
 impl Model<'_> {
-    fn new(population: Population, fitness: &dyn Fn(Individual) -> i32) -> Model {
+    fn new<'a>(population: Population<'a>, fitness: &'a dyn Fn(Individual) -> i32) -> Model<'a> {
         Model {
             population,
             fitness,
@@ -69,15 +72,19 @@ mod tests {
 
     #[test]
     fn gene() {
-        let gene = Gene::new(vec![1, 2, 3]);
+        let gene = Gene::new("Test", vec![1, 2, 3]);
         let expected = vec![1, 2, 3];
 
         assert_eq!(&gene.data, &expected);
+        assert_eq!(gene.name, "Test");
     }
 
     #[test]
     fn individual() {
-        let individual = Individual::new(vec![Gene::new(vec![1, 2, 3]), Gene::new(vec![1, 2, 3])]);
+        let individual = Individual::new(vec![
+            Gene::new("Gene0", vec![1, 2, 3]),
+            Gene::new("Gene1", vec![1, 2, 3]),
+        ]);
 
         // TODO: implement equality
         let expected = vec![1, 2, 3, 1, 2, 3];
@@ -88,13 +95,20 @@ mod tests {
             .collect();
 
         assert_eq!(res, expected);
+        assert_eq!(individual.fitness, None);
     }
 
     #[test]
     fn population() {
         let population = Population::new(vec![
-            Individual::new(vec![Gene::new(vec![1, 2, 3]), Gene::new(vec![1, 2, 3])]),
-            Individual::new(vec![Gene::new(vec![1, 2, 3]), Gene::new(vec![1, 2, 3])]),
+            Individual::new(vec![
+                Gene::new("Gene0", vec![1, 2, 3]),
+                Gene::new("Gene1", vec![1, 2, 3]),
+            ]),
+            Individual::new(vec![
+                Gene::new("Gene0", vec![1, 2, 3]),
+                Gene::new("Gene1", vec![1, 2, 3]),
+            ]),
         ]);
 
         // TODO: implement equality
@@ -106,6 +120,7 @@ mod tests {
             .collect();
 
         assert_eq!(res, expected);
+        assert_eq!(population.generation, 0);
     }
 
     #[test]
@@ -116,8 +131,14 @@ mod tests {
 
         let model = Model::new(
             Population::new(vec![
-                Individual::new(vec![Gene::new(vec![1, 2, 3]), Gene::new(vec![1, 2, 3])]),
-                Individual::new(vec![Gene::new(vec![1, 2, 3]), Gene::new(vec![1, 2, 3])]),
+                Individual::new(vec![
+                    Gene::new("Gene0", vec![1, 2, 3]),
+                    Gene::new("Gene1", vec![1, 2, 3]),
+                ]),
+                Individual::new(vec![
+                    Gene::new("Gene0", vec![1, 2, 3]),
+                    Gene::new("Gene1", vec![1, 2, 3]),
+                ]),
             ]),
             &fitness,
         );
@@ -132,5 +153,10 @@ mod tests {
             .collect();
 
         assert_eq!(res_pop_genes, exp_pop_genes);
+        // TODO: update after fitness fn is implemented
+        assert_eq!(
+            (model.fitness)(Individual::new(vec![Gene::new("Gene0", vec![1, 2, 3])])),
+            0
+        );
     }
 }
