f960a0c4ab3cdee5e5980321f8148a89940c2f0a
Chris Pryer <14341145+cnpryer@users.noreply.github.com>
2022-07-31 16:01:07 -0400

    Refactor solver struct (#8)

---
+++ src/ga/solver.rs
@@ -0,0 +1,58 @@
+use crate::ga::model::Model;
+
+const SOLVER_NAME: &str = "GeneticAlgorithm";
+
+pub struct Solver<'a> {
+    name: &'a str,
+    model: Model<'a>,
+}
+
+impl Solver<'_> {
+    pub fn new(model: Model<'_>) -> Solver {
+        Solver {
+            name: SOLVER_NAME,
+            model,
+        }
+    }
+
+    pub fn get_name(&self) -> &str {
+        self.name
+    }
+
+    pub fn get_model(&self) -> &Model {
+        &self.model
+    }
+
+    pub fn solve(&self) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::ga::{config::Config, individual::Individual, *},
+    };
+
+    fn mock_fitness_fn(individual: &Individual) -> i32 {
+        // TODO
+        individual.get_fitness().clone()
+    }
+
+    #[test]
+    fn test_solver() {
+        let model = model::Model::new(
+            population::Population::new(
+                0,
+                vec![
+                    Individual::new(vec![1, 2, 3], i32::MIN),
+                    Individual::new(vec![1, 2, 3], i32::MIN),
+                ],
+            ),
+            &mock_fitness_fn,
+            Config::default(),
+        );
+        let test_solver = Solver::new(model);
+
+        assert_eq!(test_solver.name, SOLVER_NAME);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,27 @@ pub mod ga;
 
 #[cfg(test)]
 mod tests {
+    use crate::ga::{
+        config::Config, individual::Individual, model::Model, population::Population,
+        solver::Solver,
+    };
+
+    fn mock_fitness_fn(individual: &Individual) -> i32 {
+        // TODO
+        individual.get_fitness().clone()
+    }
 
     #[test]
     fn solve_schedule() {
-        assert!(true);
+        let model = Model::new(
+            Population::new(0, vec![Individual::new(vec![1, 2, 3], i32::MIN)]),
+            &mock_fitness_fn,
+            Config::default(),
+        );
+
+        let solver = Solver::new(model);
+
+        // TODO
+        solver.solve();
     }
 }
