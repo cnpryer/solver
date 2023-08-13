886b1c21a3f91f4e0fb72f63114729620da7f5c2
Chris Pryer <cnpryer@gmail.com>
2023-08-13 00:29:20 -0400

    Update lib.rs

--- crates/solver_graph/src/lib.rs
+++ crates/solver_graph/src/lib.rs
@@ -1,3 +1,23 @@
+///! # solver-graph
+///!
+///! `Graph` can be used for operations on `Nodes` and `Edges`.
+///!
+///! ```rust
+///! struct Graph<T, U: Into<usize>> {
+///!     nodes: Nodes<T>,
+///!     edges: Edges<U, T>,
+///! }
+///! ```
+///!
+///! Constructing a `Graph` requires using the `graph!` macro to index `nodes` and `edges`.
+///!
+///! ```rust
+///! use solve_graph::{graph, nodes, edges};
+///!
+///! let nodes = nodes(vec![0, 1, 2]);
+///! let edges = edges(vec![Some(vec![edge(0, 1), edge(0, 2)]), Some(vec![edge(1, 2)]), None]);
+///! let graph = graph![nodes, edges];
+///! ```
 #[derive(Debug)]
 struct Graph<T, U: Into<usize>> {
     nodes: Nodes<T>,
